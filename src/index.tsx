20f5e25a22f4b2bdc3b47154cca154eaa3654373
Brian Truong <brian.h.t@outlook.com>
2022-09-27 21:43:37 -0700

    Add PWA functionality

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
+import { register } from "./service-register";
 
 // Bootstrap the React app
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+// Register the service worker
+register();
