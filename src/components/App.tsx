3675e2c7fe7c9c508e4c6c1df9086c176eff4c33
Brian Truong <brian.h.t@outlook.com>
2022-09-29 21:40:44 -0700

    Add theming

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,59 @@
 /** @jsxImportSource @emotion/react */
 
-import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { ThemeProvider, createTheme, ThemeOptions } from "@mui/material/styles";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Home from "./Home";
 import NotFound from "./NotFound";
+
+const theme: ThemeOptions = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#c62828",
+    },
+    secondary: {
+      main: "#9e9e9e",
+    },
+    error: {
+      main: "#f50057",
+    },
+  },
+  typography: {
+    fontFamily: "Roboto Flex",
+    h1: {
+      fontFamily: "Oswald",
+    },
+    h2: {
+      fontFamily: "Oswald",
+    },
+    h3: {
+      fontFamily: "Oswald",
+    },
+    h4: {
+      fontFamily: "Oswald",
+    },
+    h5: {
+      fontFamily: "Oswald",
+    },
+    h6: {
+      fontFamily: "Oswald",
+    },
+    button: {
+      fontFamily: "Oswald",
+    },
+    overline: {
+      fontFamily: "Oswald",
+    },
+  },
+});
 
 /**
  * App component to instantiate global providers and page routes
  */
 const App = () => {
   return (
-    <ThemeProvider theme={createTheme()}>
+    <ThemeProvider theme={theme}>
       <BrowserRouter>
         <Routes>
           <Route path="*" element={<NotFound />} />
