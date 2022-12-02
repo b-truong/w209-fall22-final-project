2ea37f5df10b005c5e73867a2da1dc1eb5c95e1d
Brian Truong <brian.h.t@outlook.com>
2022-12-02 11:40:37 -0800

    Add usePrediction

---
+++ src/components/usePrediction.ts
@@ -0,0 +1,76 @@
+import { useCallback, useEffect, useState } from "react";
+
+interface IUsePredictionOptions {
+  red?: string;
+  blue?: string;
+  class?: string;
+  rounds?: number;
+  boutType?: string;
+}
+
+interface IUsePredictionResult {
+  result?: {
+    red: number;
+    blue: number;
+  };
+  isLoading?: boolean;
+  errorMessage?: string;
+  retry?: () => void;
+}
+
+const usePrediction = (
+  options: IUsePredictionOptions
+): IUsePredictionResult => {
+  const [result, setResult] = useState<IUsePredictionResult["result"]>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<string>();
+
+  const getData = useCallback(() => {
+    // Do not perform request if parameters are missing
+    if (Object.values(options).filter((o) => !!o).length < 5) {
+      return;
+    }
+
+    const getData = async () => {
+      // Send request
+      const response = await fetch("http://api.mma.arcane-arts.net/predict", {
+        method: "GET",
+        body: JSON.stringify(options),
+      });
+
+      // Handle result
+      const body = await response.json();
+      if (response.ok) {
+        setResult(body);
+      } else {
+        setErrorMessage(body.error);
+      }
+
+      setIsLoading(false);
+    };
+
+    // Perform request and handle any other errors
+    setIsLoading(true);
+    getData().catch((error) => {
+      setErrorMessage(error);
+      setIsLoading(false);
+    });
+  }, [
+    options.red,
+    options.blue,
+    options.class,
+    options.rounds,
+    options.boutType,
+  ]);
+
+  useEffect(() => getData(), [getData]);
+
+  return {
+    result,
+    isLoading,
+    errorMessage,
+    retry: getData,
+  };
+};
+
+export default usePrediction;
